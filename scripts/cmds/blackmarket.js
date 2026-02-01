beef1f01984db06a1b6233432481389572a145cb
Azadx69x <nezukojerry803@gmail.com>
2026-02-01 21:53:17 +0600

    Update blackmarket.js

--- scripts/cmds/blackmarket.js
+++ scripts/cmds/blackmarket.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-const cmdsInfoUrl = "https://raw.githubusercontent.com/Azadwebapi/Azadx69x-blackmarket-top/main/cmdsinfo.json";
-const cmdsUrlJson = "https://raw.githubusercontent.com/Azadwebapi/Azadx69x-blackmarket-top/main/cmdsurl.json";
+const cmdsInfoUrl = "https://raw.githubusercontent.com/Azadwebapi/Azadx69x-bm-store/main/cmdsinfo.json";
+const cmdsUrlJson = "https://raw.githubusercontent.com/Azadwebapi/Azadx69x-bm-store/main/cmdsurl.json";
 const ITEMS_PER_PAGE = 10;
 
 module.exports = {
